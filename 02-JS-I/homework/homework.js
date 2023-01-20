de2b5547d976e744809c638a3245f4947c52ed93
JhonatanDzG <gizudajhovi@gmail.com>
2023-01-20 14:51:34 -0500

    COMPLETED

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -13,11 +13,10 @@ const nuevoBool = false;
 const nuevaResta = 10 - 5 === 5;
 
 // Resuelve el siguiente problema matemático:
-const nuevaMultiplicacion = 10 * 4 === 40 ;
+const nuevaMultiplicacion = 10 * 4 === 40;
 
 // Resuelve el siguiente problema matemático:
 const nuevoModulo = 21 % 5 === 1;
-
 
 // En los próximos 22 problemas, deberás completar la función.
 // Todo tu código irá dentro de las llaves de la función.
@@ -41,7 +40,7 @@ function suma(x, y) {
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
-  return x - y;  
+  return x - y;
 }
 
 function multiplica(x, y) {
@@ -60,36 +59,40 @@ function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-  if(x === y){
+  if (x === y) {
     return true;
-  }return false;
+  }
+  return false;
 }
 
 function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  if(str1.length === str2.length){
+  if (str1.length === str2.length) {
     return true;
-  }return false;
+  }
+  return false;
 }
 
 function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
-  if(num < 90){
+  if (num < 90) {
     return true;
-  }return false;
+  }
+  return false;
 }
 
 function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
-  if(num > 50){
+  if (num > 50) {
     return true;
-  }return false;
+  }
+  return false;
 }
 
 function obtenerResto(x, y) {
@@ -102,18 +105,20 @@ function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
-  if(num % 2 == 0){
+  if (num % 2 == 0) {
     return true;
-  }return false;
+  }
+  return false;
 }
 
 function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
-  if(num % 2 != 0){
+  if (num % 2 != 0) {
     return true;
-  }return false;
+  }
+  return false;
 }
 
 function elevarAlCuadrado(num) {
@@ -126,7 +131,7 @@ function elevarAlCuadrado(num) {
 function elevarAlCubo(num) {
   // Devuelve el valor de "num" elevado al cubo
   // Tu código:
-  return num*num*num;
+  return num * num * num;
 }
 
 function elevar(num, exponent) {
@@ -134,11 +139,11 @@ function elevar(num, exponent) {
   // Tu código:
 
   let acum = num;
-  for(i = 1; i < exponent ; i++){
+  for (i = 1; i < exponent; i++) {
     let inP = num * acum;
     acum = inP;
   }
-  return acum; 
+  return acum;
 }
 
 function redondearNumero(num) {
@@ -147,9 +152,9 @@ function redondearNumero(num) {
   let resto = num % 1;
   let entero = num - resto;
 
-  if(resto >= 0.5){
+  if (resto >= 0.5) {
     return entero + 1;
-  }else {
+  } else {
     return entero;
   }
 }
@@ -160,9 +165,9 @@ function redondearHaciaArriba(num) {
   let resto = num % 1;
   entero = num - resto;
 
-  if(resto != 0 ){
+  if (resto != 0) {
     return entero + 1;
-  }else {
+  } else {
     return num;
   }
 }
@@ -174,7 +179,7 @@ function numeroRandom() {
 }
 
 function esPositivo(numero) {
-  //La función va a recibir un entero. Devuelve como resultado una cadena de texto que indica si el número es positivo o negativo. 
+  //La función va a recibir un entero. Devuelve como resultado una cadena de texto que indica si el número es positivo o negativo.
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
@@ -182,11 +187,12 @@ function esPositivo(numero) {
   let negativo = "Es negativo";
   let zero = false;
 
-  if(numero > 0 ){
+  if (numero > 0) {
     return positivo;
-  }else if(numero < 0){
+  } else if (numero < 0) {
     return negativo;
-  }return zero;
+  }
+  return zero;
 }
 
 function agregarSimboloExclamacion(str) {
@@ -213,47 +219,47 @@ function obtenerSaludo(nombre) {
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
-  return (alto * ancho);
+  return alto * ancho;
 }
 
-
-function retornarPerimetro(lado){
+function retornarPerimetro(lado) {
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
   return lado * 4;
 }
 
-
-function areaDelTriangulo(base, altura){
+function areaDelTriangulo(base, altura) {
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
   return (base * altura) / 2;
 }
 
-
-function deEuroAdolar(euro){
+function deEuroAdolar(euro) {
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  dolar = 1.20;
+  dolar = 1.2;
 
   return euro * dolar;
 }
 
-
-function esVocal(letra){
-  //Escribe una función que reciba una letra y, si es una vocal, muestre el mensaje “Es vocal”. 
-  //Verificar si el usuario ingresó un string de más de un carácter, en ese caso, informarle 
+function esVocal(letra) {
+  //Escribe una función que reciba una letra y, si es una vocal, muestre el mensaje “Es vocal”.
+  //Verificar si el usuario ingresó un string de más de un carácter, en ese caso, informarle
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  if(letra === "a" || letra ==="e" || letra === "i" || letra === "o" || letra === "u"){
-    return "Es vocal"
-  }return "Dato incorrecto"
-
+  if (
+    letra === "a" ||
+    letra === "e" ||
+    letra === "i" ||
+    letra === "o" ||
+    letra === "u"
+  ) {
+    return "Es vocal";
+  }
+  return "Dato incorrecto";
 }
-
-
 
 // No modificar nada debajo de esta línea
 // --------------------------------
